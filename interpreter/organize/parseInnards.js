2f39d5f230d39c36975179352027748a1b791fbb
border-l <borderlexum@gmail.com>
2025-04-18 10:15:00 -0400

    Parsing vectors in <VECTORS> the same as in assignments

--- interpreter/organize/parseInnards.js
+++ interpreter/organize/parseInnards.js
@@ -8,6 +8,7 @@ const getArray = require("../types/getArray")
 const ThrowError = require("../errors/ThrowError")
 const checkVariableName = require("../helpers/checkVariableName")
 const asnOperators = require('./operators')
+const getBalanced = require('../helpers/getBalanced')
 
 // Parses the bodies of functions & macro section (split it up)
 function parseInnards(context, index, section) {
@@ -145,27 +146,6 @@ function parseInnards(context, index, section) {
 
     // Parsed block and final index
     return [parsed, finalIndex]
-}
-
-// Gets balanced expr, with index being the beginning of it in array, starting with "("
-function getBalanced(index, array) {
-    let i
-    let str = ""
-    let balanced = 0
-
-    for (i = index; i < array.length; i++) {
-        token = array[i]
-
-        for (let x = 0; x < token.length; x++) {
-            if (token[x] === "(") balanced -= 1
-            else if (token[x] === ")") balanced += 1
-        }
-
-        str += " " + token
-        if (balanced === 0) return [str, i]
-    }
-
-    return [str, i]
 }
 
 module.exports = parseInnards
--- interpreter/sections/parseVectors.js
+++ interpreter/sections/parseVectors.js
@@ -2,15 +2,21 @@ const parseSection = require("./parseSection")
 const ThrowError = require("../errors/ThrowError")
 const getVariables = require("../types/getVariables")
 const checkVariableName = require("../helpers/checkVariableName")
-const combineTillNext = require('../helpers/combineTillNext')
+const getArray = require("../types/getArray")
+const evaluateExpr = require("../helpers/evaluateExpr")
+const getBalanced = require('../helpers/getBalanced')
 
 // Handles Vectors section
-function parseVectors (context) {
+function parseVectors(context) {
     parseSection(context, (tokens, token, i, section, next) => {
         // Check the variable name is valid
         if (!checkVariableName(token)) {
             ThrowError(1100, { AT: token })
         }
+
+        if (i >= context.tokens.length - 3) {
+            // Some error, too few for assignment
+        } 
 
         // Get list of variables and check for duplicates
         const others = getVariables(context)
@@ -18,51 +24,100 @@ function parseVectors (context) {
             ThrowError(1500, { AT: token })
         }
 
-        // Get assignment
-        const [assignment, index] = combineTillNext(context, "$", i, false, true)
-        // Error if assignment operator is not present where it should be
-        if (assignment.charAt(0) !== "=" || context.tokens[i + 1] !== "=") {
+        if (context.tokens[i + 1] !== "=") {
             ThrowError(1600, { AT: token, SECTION: "VECTORS" })
         }
 
-        // Separate expression
-        const [nums, input, ...rest] = assignment.replace("=", "").split("|")
+        let value
+        let stopIndex
 
-        // Shouldnt exist
-        if (rest.length > 0) {
-            ThrowError(2400, { AT: token })
-        }
-        // Validate input if it is there
-        if (input !== undefined && input !== "input") {
-                ThrowError(2400, { AT: token })
-        }
-
-        // Get numbers in vector
-        const numbers = nums.split(",").map((val) => val.length === 0 ? 0 : Number(val))
-        // Error if there are no numbers
-        if (numbers.length === 0) {
-            ThrowError(2400, { AT: token })
+        if (context.tokens[i + 2][0] === "[") {
+            const [vector, newIndex] = getArray(context, i + 2, true)
+            for (let x = 0; x < vector.length; x++) {
+                vector[x] = evaluateExpr(context, vector[x])
+            }
+            value = vector
+            stopIndex = newIndex
         }
 
-        // Error if invalid number present
-        if (numbers.some((val) => isNaN(val))) {
-            ThrowError(2405, { AT: token })
+        else if (context.tokens[i + 2][0] === "(") {
+            const [expr, newIndex] = getBalanced(i + 2, context.tokens)
+            value = [evaluateExpr(context, expr), 0]
+            stopIndex = newIndex
         }
 
-        // Put in inputVectors if that was an option
-        if (input !== undefined) {
-            context.inputVectors[token] = numbers
+        else {
+            value = [evaluateExpr(context, context.tokens[i + 2]), 0]
+            stopIndex = i + 2
         }
 
-        // Assume other value is 0 if only one number
-        if (numbers.length === 1) {
-            context.model.VECTORS[token] = [numbers[0], 0]
-            return index - 1
+        if (context.tokens[stopIndex + 1] === "|" && context.tokens[stopIndex + 2] === "INPUT") {
+            context.inputVectors[token] = value
+            stopIndex += 2
         }
-        // Update & move past assignment
-        context.model.VECTORS[token] = numbers
-        return index - 1
+
+        context.model.VECTORS[token] = value
+        return stopIndex
     }, (section) => section === "VECTORS")
 }
+// {
+//     parseSection(context, (tokens, token, i, section, next) => {
+//         // Check the variable name is valid
+//         if (!checkVariableName(token)) {
+//             ThrowError(1100, { AT: token })
+//         }
+
+//         // Get list of variables and check for duplicates
+//         const others = getVariables(context)
+//         if (others.includes(token)) {
+//             ThrowError(1500, { AT: token })
+//         }
+
+//         // Get assignment
+//         const [assignment, index] = combineTillNext(context, "$", i, false, true)
+//         // Error if assignment operator is not present where it should be
+//         if (assignment.charAt(0) !== "=" || context.tokens[i + 1] !== "=") {
+//             ThrowError(1600, { AT: token, SECTION: "VECTORS" })
+//         }
+
+//         // Separate expression
+//         const [nums, input, ...rest] = assignment.replace("=", "").split("|")
+
+//         // Shouldnt exist
+//         if (rest.length > 0) {
+//             ThrowError(2400, { AT: token })
+//         }
+//         // Validate input if it is there
+//         if (input !== undefined && input !== "input") {
+//             ThrowError(2400, { AT: token })
+//         }
+
+//         // Get numbers in vector
+//         const numbers = nums.split(",").map((val) => val.length === 0 ? 0 : Number(val))
+//         // Error if there are no numbers
+//         if (numbers.length === 0) {
+//             ThrowError(2400, { AT: token })
+//         }
+
+//         // Error if invalid number present
+//         if (numbers.some((val) => isNaN(val))) {
+//             ThrowError(2405, { AT: token })
+//         }
+
+//         // Put in inputVectors if that was an option
+//         if (input !== undefined) {
+//             context.inputVectors[token] = numbers
+//         }
+
+//         // Assume other value is 0 if only one number
+//         if (numbers.length === 1) {
+//             context.model.VECTORS[token] = [numbers[0], 0]
+//             return index - 1
+//         }
+//         // Update & move past assignment
+//         context.model.VECTORS[token] = numbers
+//         return index - 1
+//     }, (section) => section === "VECTORS")
+// }
 
 module.exports = parseVectors
